cb82d7a1fac25c71bebe701fdc0ff98a6d5d470a
Alessandro <marzucoalessandro1@gmail.com>
2020-11-20 14:54:39 +0100

    inizio

---
+++ assets/js/palindroma.js
@@ -0,0 +1,7 @@
+//Palidroma
+//Chiedere all’utente di inserire una parola
+//Creare una funzione per capire se la parola inserita è palindroma
+
+//1 creiamo una var col prompt per chiedere una parola all'utente
+
+//2 creiamo una funziona che esamini la parola inserita e all'interno un if per vedere se la parola capovolta ha lo stesso valore.
