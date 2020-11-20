ea790e9fd7fa93993feb6be6edfad6c9c0913f18
Alessandro <marzucoalessandro1@gmail.com>
2020-11-20 15:58:19 +0100

    provo i metodi fuori dalla funzione

--- assets/js/palindroma.js
+++ assets/js/palindroma.js
@@ -5,3 +5,29 @@
 //1 creiamo una var col prompt per chiedere una parola all'utente
 
 //2 creiamo una funziona che esamini la parola inserita e all'interno un if per vedere se la parola capovolta ha lo stesso valore.
+
+//3 per fare qst dobbiamo:
+//3.1 trasformare la ns parola in un array
+//3.2 rovesciare tutti gli elementi all'interno dell'array
+//3.3 trasformare di nuovo in stringa il nostro array:
+
+var parola = prompt("dimmi una parola");
+console.log(parola);
+
+var newarray = parola.split("");
+console.log(newarray);
+
+var reversearray = newarray.reverse();
+console.log(reversearray);
+
+var nuova = reversearray.join("");
+console.log(nuova);
+
+function palindroma(text) {
+
+
+
+
+
+  console.log(newarray);
+}
