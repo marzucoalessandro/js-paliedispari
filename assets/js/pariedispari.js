30bf3dd9feae15d23614a00da0a74586daf31801
Alessandro <marzucoalessandro1@gmail.com>
2020-11-20 17:56:22 +0100

    pari e dispari

--- assets/js/pariedispari.js
+++ assets/js/pariedispari.js
@@ -4,15 +4,38 @@
 //Sommiamo i due numeri
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 //Dichiariamo chi ha vinto.
-var pariodispari = prompt("scegli pari con p o dispari con d");
-if (pariodispari !== "p" || pariodispari !== "d") {
-  alert("lettera sbagliata");
 
-}
+var letter = prompt("scegli pari con p o dispari con d");
+
+//if (letter = p) {
+  //  letter = true;
+    //alert("hai scelto pari ");
+  //} else if (letter = d) {
+    //letter = true;
+    //alert("hai scelto dispari");
+  //} else {
+    //alert("devi scegliere p oppure d");
+  //}
+
+
+
 
 var choosenumber = Number(prompt("scegli un numero tra 1 e 5"));
 
-if (choosenumber > "5" && choosenumber <= "0") {
+if ((choosenumber > 5) || (choosenumber <= 0)) {
     alert("il numero inserito è sbagliato");
 
 }
+
+//creaimo la funzione per il numero casuale tra 1 e 5 del comp;
+
+ function numrandom(number) {
+
+   return Math.floor(Math.random() * (5 - 1));
+ }
+
+ // assegniamo una variabile alla funzione numrandom;
+ var number = numrandom(number);
+
+ var somma =  number + choosenumber;
+ 
